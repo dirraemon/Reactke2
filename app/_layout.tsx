0c3512c425ce2af7086eee52b73baf4345bcc942
dirraemon <dirafitriannisa05@gmail.com>
2026-02-25 11:15:37 +0700

    update font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,33 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    PoppinsRegular: require('../assets/font/Poppins-Regular.ttf'),
+    PoppinsBold: require('../assets/font/Poppins-Bold.ttf'),
+  });
+
+   useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
--- components/themed-text.tsx
+++ components/themed-text.tsx
@@ -35,24 +35,28 @@ export function ThemedText({
 
 const styles = StyleSheet.create({
   default: {
+    fontFamily: 'PoppinsRegular',
     fontSize: 16,
     lineHeight: 24,
   },
   defaultSemiBold: {
+    fontFamily: 'PoppinsBold',
     fontSize: 16,
     lineHeight: 24,
     fontWeight: '600',
   },
   title: {
+    fontFamily: 'PoppinsBold',
     fontSize: 32,
     fontWeight: 'bold',
     lineHeight: 32,
   },
   subtitle: {
+    fontFamily: 'PoppinsBold',
     fontSize: 20,
     fontWeight: 'bold',
   },
-  link: {
+  link: { 
     lineHeight: 30,
     fontSize: 16,
     color: '#0a7ea4',
